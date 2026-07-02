7a6b4a9e76bfc4df236c04761a52075f587f0dad
trobasuj <makar9922@gmail.com>
2026-07-02 18:28:04 +0500

    feat(sitemap): add second index specifically for the final two files to bypass GSC truncation bug

---
+++ frontend/src/app/sitemap-index-2.xml/route.js
@@ -0,0 +1,25 @@
+export async function GET() {
+  const sitemaps = [
+    'https://openangels.xyz/sitemap-investors-5.xml',
+    'https://openangels.xyz/sitemap-static.xml',
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${sitemaps
+    .map(
+      (url) => `
+  <sitemap>
+    <loc>${url}</loc>
+  </sitemap>`
+    )
+    .join('')}
+</sitemapindex>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=86400, s-maxage=86400, stale-while-revalidate',
+    },
+  });
+}
