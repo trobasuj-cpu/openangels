34b2ba70ad87b06ceb249203c315c777222f719c
trobasuj <makar9922@gmail.com>
2026-07-02 17:50:03 +0500

    fix(sitemap): replace dynamic query routing with explicit static routes to guarantee GSC compatibility

---
+++ frontend/src/app/sitemap-index.xml/route.js
@@ -0,0 +1,29 @@
+export async function GET() {
+  const sitemaps = [
+    'https://openangels.xyz/sitemap-static.xml',
+    'https://openangels.xyz/sitemap-investors-1.xml',
+    'https://openangels.xyz/sitemap-investors-2.xml',
+    'https://openangels.xyz/sitemap-investors-3.xml',
+    'https://openangels.xyz/sitemap-investors-4.xml',
+    'https://openangels.xyz/sitemap-investors-5.xml',
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${sitemaps
+    .map(
+      (url) => `
+  <sitemap>
+    <loc>${url}</loc>
+  </sitemap>`
+    )
+    .join('')}
+</sitemapindex>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=86400, s-maxage=86400, stale-while-revalidate',
+    },
+  });
+}
