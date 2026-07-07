c4d5419ec263b1307b51b7ab7fa510602c94cf53
trobasuj <makar9922@gmail.com>
2026-07-07 16:38:27 +0500

    fix(seo): manually create sitemap.xml index to point to Next.js 14 dynamically generated sitemaps

---
+++ frontend/src/app/sitemap.xml/route.js
@@ -0,0 +1,28 @@
+export async function GET() {
+  const sitemaps = [
+    'https://openangels.xyz/sitemap/static.xml',
+    'https://openangels.xyz/sitemap/0.xml',
+    'https://openangels.xyz/sitemap/1.xml',
+    'https://openangels.xyz/sitemap/2.xml',
+    'https://openangels.xyz/sitemap/3.xml',
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${sitemaps
+    .map(
+      (url) => `
+  <sitemap>
+    <loc>${url}</loc>
+  </sitemap>`
+    )
+    .join('')}
+</sitemapindex>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=86400, s-maxage=86400, stale-while-revalidate',
+    },
+  });
+}
