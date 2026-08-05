32d5bca0a4d2c4116e8ac2c7726f7921cf2b0e7e
trobasuj <makar9922@gmail.com>
2026-08-05 13:42:42 +0500

    fix(seo): restore sitemap-index.xml GET handler to fix 405 HTTP error in Google Search Console

---
+++ frontend/src/app/sitemap-index.xml/route.js
@@ -0,0 +1,24 @@
+export async function GET() {
+  const sitemaps = [
+    'https://openangels.xyz/sitemap-static.xml',
+    'https://openangels.xyz/sitemap-1.xml',
+    'https://openangels.xyz/sitemap-2.xml',
+    'https://openangels.xyz/sitemap-3.xml',
+    'https://openangels.xyz/sitemap-4.xml',
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${sitemaps.map((url) => `  <sitemap>
+    <loc>${url}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>`).join('\n')}
+</sitemapindex>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600, s-maxage=86400, stale-while-revalidate=86400',
+    },
+  });
+}
